03b74a867a11ce55df4af3bb607af73294bb45c9
Michael Bromley <michael@michaelbromley.co.uk>
2015-03-24 14:12:15 +0100

    add live reload capability to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,8 @@ var gulp = require('gulp'),
     ngAnnotate = require('gulp-ng-annotate'),
     jsHint = require('gulp-jshint'),
     inject = require('gulp-inject'),
-    templateCache = require('gulp-angular-templatecache');
+    templateCache = require('gulp-angular-templatecache'),
+    livereload = require('gulp-livereload');
 
 
 var VENDOR_SCRIPTS = [
@@ -23,7 +24,8 @@ gulp.task('app-scripts', function() {
    ])
        .pipe(jsHint())
        .pipe(ngAnnotate())
-       .pipe(gulp.dest('build/app'));
+       .pipe(gulp.dest('build/'))
+       .pipe(livereload());
 });
 
 gulp.task('vendor-scripts', function() {
@@ -34,7 +36,8 @@ gulp.task('vendor-scripts', function() {
 gulp.task('app-templates', function() {
     return gulp.src('src/app/**/*.html')
         .pipe(templateCache('templates.js', { module: 'caiLunAdminUi'}))
-        .pipe(gulp.dest('build/app/'));
+        .pipe(gulp.dest('build/app/'))
+        .pipe(livereload());
 });
 
 gulp.task('vendor-styles', function() {
@@ -52,14 +55,25 @@ gulp.task('index', ['app-scripts', 'app-templates', 'vendor-scripts', 'vendor-st
             '**/*.js',
             '**/*.css'
         ], { cwd: 'build/'} ),  { addRootSlash: false }))
-        .pipe(gulp.dest('build/'));
+        .pipe(gulp.dest('build/'))
+        .pipe(livereload());
 });
 
 gulp.task('static-assets', function() {
     return gulp.src([
         'src/.htaccess'
     ])
-        .pipe(gulp.dest('build/'));
+        .pipe(gulp.dest('build/'))
+        .pipe(livereload());
+});
+
+gulp.task('watch', ['index'], function() {
+    livereload.listen();
+    gulp.watch('src/app/**/*.js', ['app-scripts']);
+    gulp.watch('src/app/**/*.html', ['app-templates']);
+    //gulp.watch('src/less/*.less', ['styles']);
+    gulp.watch('src/index.html', ['index']);
+    //gulp.watch(['src/assets/**/*.*', 'src/app/.htaccess', 'src/app/static-page.php'], ['static-assets']);
 });
 
 gulp.task('default', ['index', 'static-assets']);
