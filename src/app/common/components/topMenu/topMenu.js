fffe4bdf809b8eb5166009e3e8ad17ca8e8683b5
Michael Bromley <michael@michaelbromley.co.uk>
2015-03-30 13:44:46 +0200

    Add mock user info to topMenu

--- src/app/common/components/topMenu/topMenu.js
+++ src/app/common/components/topMenu/topMenu.js
@@ -5,6 +5,10 @@ function topMenuDirective(authService) {
     function topMenuLinkFn(scope) {
         scope.isLoggedIn = isLoggedIn;
 
+        // TODO: get this data from the backend, not hard-coded
+        scope.userName = 'John Smith';
+        scope.profileImage = 'assets/images/example-profile-pic.jpg';
+
         authService.onLogIn(function() {
             scope.isLoggedIn = true;
         });
