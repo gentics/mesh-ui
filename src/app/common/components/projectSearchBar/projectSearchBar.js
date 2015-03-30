41ae791d1ae9e30db1cfe77bfd0dd8b5acd3c8ca
Michael Bromley <michael@michaelbromley.co.uk>
2015-03-30 12:04:28 +0200

    Add project search bar component

---
+++ src/app/common/components/projectSearchBar/projectSearchBar.js
@@ -0,0 +1,19 @@
+function projectSearchBarDirective() {
+
+    function projectSearchBarLinkFn(scope) {
+        // hard-coded values for now
+        // TODO: replace with call to a backend service to get this data
+
+        scope.projects = ['Project One', 'Project Two'];
+        scope.currentContext = "";
+    }
+
+    return {
+        restrict: 'E',
+        templateUrl: 'common/components/projectSearchBar/projectSearchBar.html',
+        link: projectSearchBarLinkFn
+    };
+}
+
+angular.module('caiLunAdminUi')
+    .directive('projectSearchBar', projectSearchBarDirective);
