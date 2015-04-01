e65d072ceb40c39688b7b5f1df881b62f234ee50
Michael Bromley <michael@michaelbromley.co.uk>
2015-04-01 14:32:16 +0200

    Refactor dataService and add doc blocks

--- src/app/common/services/dataService.js
+++ src/app/common/services/dataService.js
@@ -2,47 +2,81 @@ angular.module('caiLunAdminUi.common')
     .config(dataServiceConfig)
     .provider('dataService', dataServiceProvider);
 
+/**
+ * The dataServiceProvider is used to configure and create the dataService which is used
+ * for all requests to the API.
+ */
 function dataServiceProvider() {
 
     var apiUrl;
 
-    this.setApiUrl = function(value) {
-        apiUrl = value;
-    };
+    this.setApiUrl = setApiUrl;
+    this.$get = dataService;
 
-    this.$get = function dataService(Restangular) {
+    /**
+     * Allow config of the API url in the app's config phase.
+     * @param value
+     */
+    function setApiUrl(value) {
+        apiUrl = value;
+    }
+
+    /**
+     * The data service itself which is responsible for all requests to the API.
+     *
+     * @param Restangular
+     * @returns {{getProjects: getProjects}}
+     */
+    function dataService(Restangular) {
 
         Restangular.setBaseUrl(apiUrl);
-
         var projects = Restangular.all('projects');
+        var users = Restangular.all('users');
 
         function getProjects() {
             return projects.getList();
         }
+        function getUsers() {
+            return users.getList();
+        }
 
         return {
-            getProjects: getProjects
+            getProjects: getProjects,
+            getUsers: getUsers
         };
-    };
+    }
 }
 
+/**
+ * Configure Restangular
+ *
+ * @param RestangularProvider
+ */
 function dataServiceConfig(RestangularProvider) {
     // basic auth credentials: joe1:test123
     // header string: Authorization: Basic am9lMTp0ZXN0MTIz
     // TODO: this will be replaced by an OAuth 2 solution.
     RestangularProvider.setDefaultHeaders({ "Authorization": "Basic am9lMTp0ZXN0MTIz"});
 
-    // add a response intereceptor
-    RestangularProvider.addResponseInterceptor(function(data, operation, what, url, response, deferred) {
-        var extractedData;
-        // .. to look for getList operations
-        if (operation === "getList") {
-            // .. and handle the data and meta data
-            extractedData = data.data.data;
-            extractedData.meta = data.data.meta;
-        } else {
-            extractedData = data.data;
-        }
-        return extractedData;
-    });
+    RestangularProvider.addResponseInterceptor(restangularResponseInterceptor);
+}
+
+/**
+ * Extract the payload from the response, which is returned as the value of the "data" key.
+ *
+ * @param data
+ * @param operation
+ * @returns {Object}
+ */
+function restangularResponseInterceptor(data, operation) {
+    var extractedData;
+
+    if (operation === "getList") {
+        extractedData = data.data;
+        extractedData.metadata = data['_metainfo'];
+    } else {
+        extractedData = data;
+    }
+
+    return extractedData;
 }
