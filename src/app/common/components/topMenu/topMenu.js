dbd6e0dbb35cb359df23c25312519df8b56f9516
Michael Bromley <michael@michaelbromley.co.uk>
2015-04-07 11:30:00 +0200

    Refactor topMenu and force reload of view on language change

--- src/app/common/components/topMenu/topMenu.js
+++ src/app/common/components/topMenu/topMenu.js
@@ -1,39 +1,43 @@
 angular.module('caiLunAdminUi.common')
     .directive('topMenu', topMenuDirective);
 
-function topMenuDirective(authService, i18nService) {
+function topMenuDirective($state, authService, i18nService) {
 
     var isLoggedIn = authService.isLoggedIn;
 
-    function topMenuLinkFn(scope) {
-        scope.isLoggedIn = isLoggedIn;
-        scope.languages = i18nService.languages;
-        scope.lang = i18nService.getLanguage();
+    function topMenuController() {
+        var vm = this;
+        
+        vm.isLoggedIn = isLoggedIn;
+        vm.languages = i18nService.languages;
+        vm.lang = i18nService.getLanguage();
 
         // TODO: get this data from the backend, not hard-coded
-        scope.userName = 'John Smith';
-        scope.profileImage = 'assets/images/example-profile-pic.jpg';
+        vm.userName = 'John Smith';
+        vm.profileImage = 'assets/images/example-profile-pic.jpg';
 
-        scope.logout = function() {
+        vm.logout = function() {
             authService.logOut();
         };
 
-        scope.updateLanguage = function(lang) {
+        vm.updateLanguage = function(lang) {
             i18nService.setLanguage(lang);
+            $state.reload();
         };
 
         authService.registerLogInHandler(function() {
-            scope.isLoggedIn = true;
+            vm.isLoggedIn = true;
         });
 
         authService.registerLogOutHandler(function() {
-            scope.isLoggedIn = false;
+            vm.isLoggedIn = false;
         });
     }
 
     return {
         restrict: 'E',
         templateUrl: 'common/components/topMenu/topMenu.html',
-        link: topMenuLinkFn
+        controller: topMenuController,
+        controllerAs: 'vm'
     };
 }
