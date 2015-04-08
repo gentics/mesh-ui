5f6198f991efb331d1f88de757f8174eb7e34876
Michael Bromley <michael@michaelbromley.co.uk>
2015-04-08 13:40:45 +0200

    Changing page or items per page on project explorer persists changes to url query string.

--- src/app/projects/projectExplorer/projectExplorerController.js
+++ src/app/projects/projectExplorer/projectExplorerController.js
@@ -2,25 +2,39 @@ angular.module('caiLunAdminUi.projects')
     .controller('ProjectExplorerController', ProjectExplorerController);
 
 /**
+ * @param $location
  * @param dataService
  * @param contextService
  * @constructor
  */
-function ProjectExplorerController(dataService, contextService) {
+function ProjectExplorerController($location, dataService, contextService) {
     var vm = this;
 
-    vm.itemsPerPage = 25;
     vm.totalItems = 0;
-    vm.currentPage = 1;
+    vm.itemsPerPage = $location.search().per_page || 25;
+    vm.currentPage = $location.search().page || 1;
 
     // TODO: get this data from the server
     vm.tags = ['January News', 'February News', 'March News'];
 
+    /**
+     * Called when a page is changed via pagination - update the url query string
+     * and get the results from the dataService.
+     * @param {number} newPage
+     */
     vm.getPage = function(newPage) {
+        $location.search('page' , newPage);
         populate(newPage);
     };
 
-    populate(1);
+    /**
+     * When the number of items per page is changed, update the url query string.
+     */
+    vm.setItemsPerPage = function() {
+        $location.search('per_page' , vm.itemsPerPage);
+    };
+
+    populate(vm.currentPage);
 
     function populate(page) {
         var projectName = contextService.getProject().name,
