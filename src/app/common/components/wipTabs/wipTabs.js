386522879fcaae59067bffa8a3f6b32c24fd50db
Michael Bromley <michael@michaelbromley.co.uk>
2015-04-13 16:36:34 +0200

    Implement simple closing of wip tabs

--- src/app/common/components/wipTabs/wipTabs.js
+++ src/app/common/components/wipTabs/wipTabs.js
@@ -2,7 +2,7 @@ angular.module('caiLunAdminUi.common')
     .directive('wipTabs', wipTabs);
 
 /**
- * Directive for the top menu bar which is present on every screen apart from the login.
+ * Directive work-in-progress (WIP) tabs which allow switching between open editor views.
  *
  * @param $state
  * @param wipService
@@ -11,27 +11,61 @@ angular.module('caiLunAdminUi.common')
 function wipTabs($state, wipService) {
 
     function wipTabsController($scope) {
-        var vm = this;
+        var vm = this,
+            lastIndex = 0;
+
         vm.wips = [];
         vm.selectedIndex = 0;
 
+        /**
+         * Close a WIP tab and remove the WIP item from the wipService, automatically switching to another
+         * tab or the list view.
+         *
+         * @param {number} index
+         */
+        vm.closeWip = function(index) {
+            var wip = vm.wips[index],
+                newWip;
+            wipService.closeContent(wip);
+
+            if (0 < vm.wips.length) {
+                vm.selectedIndex = lastIndex < vm.wips.length ? lastIndex : vm.wips.length - 1;
+                newWip = vm.wips[vm.selectedIndex];
+            }
+
+            if (newWip) {
+                $state.go('projects.explorer.content', { uuid: newWip.uuid });
+            } else {
+                $state.go('projects.explorer');
+            }
+        };
+
         wipService.registerWipChangeHandler(function() {
             var updatedWips = wipService.getOpenContents();
-            for (var i = updatedWips.length - vm.wips.length; 0 < i; i--) {
-                var curr = updatedWips[updatedWips.length - i];
-                vm.wips.push(curr);
-                vm.selectedIndex = indexByUuid(vm.wips, curr.uuid);
+            console.log('number of wips: ' + updatedWips.length);
+
+            vm.wips = updatedWips;
+            vm.selectedIndex = indexByUuid(vm.wips, $state.params.uuid);
+            if (-1 < vm.selectedIndex) {
+                lastIndex = vm.selectedIndex;
             }
+
         });
 
         $scope.$on('$stateChangeSuccess', function(event, toState, toParams) {
             if (toParams && toParams.uuid) {
-                vm.selectedIndex = indexByUuid(vm.wips, toParams.uuid);
+                //vm.selectedIndex = indexByUuid(vm.wips, toParams.uuid);
             } else {
                 vm.selectedIndex = -1;
             }
         });
 
+        /**
+         * Get the index of a given WIP item in the collection by its uuid.
+         * @param {Array} collection
+         * @param {String} uuid
+         * @returns {number}
+         */
         function indexByUuid(collection, uuid) {
             return collection.map(function(wip) {
                 return wip.uuid;
@@ -43,6 +77,7 @@ function wipTabs($state, wipService) {
         restrict: 'E',
         templateUrl: 'common/components/wipTabs/wipTabs.html',
         controller: wipTabsController,
-        controllerAs: 'vm'
+        controllerAs: 'vm',
+        scope: {}
     };
 }
--- src/app/common/services/wipService.js
+++ src/app/common/services/wipService.js
@@ -1,6 +1,14 @@
 angular.module('caiLunAdminUi.common')
     .service('wipService', WipService);
 
+/**
+ * Service to track work-in-progress (WIP) items, i.e. objects which have
+ * been opened for editing but have not had their changes persisted back
+ * to the server. This service allows the user to edit multiple open objects
+ * at once, and tracks whether they need to be persisted.
+ *
+ * @constructor
+ */
 function WipService() {
     var contents = {},
         onWipChangeCallbacks = [];
@@ -10,11 +18,19 @@ function WipService() {
     this.getOpenContents = getOpenContents;
     this.registerWipChangeHandler = registerWipChangeHandler;
 
+    /**
+     * Add a new content items to the open contents store.
+     * @param {Object} content
+     */
     function openContent(content) {
         contents[content.uuid] = content;
         invokeChangeCallbacks();
     }
 
+    /**
+     * Remove an existing content item from the open contents store.
+     * @param content
+     */
     function closeContent(content) {
         if (contents[content.uuid]) {
             delete contents[content.uuid];
@@ -22,22 +38,35 @@ function WipService() {
         }
     }
 
+    /**
+     * Get an array of all open contents.
+     * @returns {Array}
+     */
     function getOpenContents() {
         var contentsArray = [];
-
         for(var key in contents) {
             if (contents.hasOwnProperty(key)) {
                 contentsArray.push(contents[key]);
             }
         }
-
         return contentsArray;
     }
 
+    /**
+     * Register a callback function which will be invoked every time there is a change to
+     * the status of wip items (i.e. whenever a new wip item is opened, or an existing one
+     * is closed).
+     *
+     * @param {Function} callback
+     */
     function registerWipChangeHandler(callback) {
         onWipChangeCallbacks.push(callback);
     }
 
+    /**
+     * Invokes any change handler callbacks which were previously registered via
+     * registerWipChangeHandler().
+     */
     function invokeChangeCallbacks() {
         onWipChangeCallbacks.forEach(function(fn) {
             fn();
