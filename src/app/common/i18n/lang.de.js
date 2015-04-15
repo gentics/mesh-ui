15151eb6850484c6f621170fe92fadd99a5a26a7
Michael Bromley <michael@michaelbromley.co.uk>
2015-04-15 16:49:11 +0200

    Add translations for existing UI text.

--- src/app/common/i18n/lang.de.js
+++ src/app/common/i18n/lang.de.js
@@ -5,10 +5,33 @@ var germanTranslations = {
     'PASSWORD': 'Kennwort',
     'PROFILE': 'Profil',
 
-    'SEARCH': 'Suche',
-
     'PROJECTS': 'Projekte',
-    'ALL_PROJECTS': 'Alle Projekte'
+    'ALL_PROJECTS': 'Alle Projekte',
+
+    /* Tags */
+    'CREATE_NEW_TAG': 'Neuen Tag erstellen',
+
+    /* Other */
+    'SEARCH': 'Suche',
+    'UPDATE': 'Aktualisieren',
+    'TITLE': 'Titel',
+    'SCHEMA': 'Schema',
+    'AUTHOR': 'Autor',
+    'CREATED': 'Erstellt',
+    'ITEMS_PER_PAGE': 'Artikel pro Seite',
+
+    /* Dialogs */
+    'CONTENT_MODIFIED': 'Inhalt geändert',
+    'CONTENT_MODIFIED_HOW_TO_PROCEED': 'Dieser Inhalt wurde geändert. Wie möchten Sie vorgehen?',
+
+    /* Common buttons */
+    'SAVE_CHANGES': 'Speichern',
+    'DISCARD_CHANGES': 'Wegwerfen',
+    'CANCEL': 'Stornieren',
+
+    /* Error messages */
+    'ERROR': 'Fehler',
+    'ERR_CHECK_LOGIN_DETAILS': 'Überprüfen Sie bitte Ihre Daten ein und versuchen Sie es erneut.'
 };
 
 angular.module('caiLunAdminUi.common.i18n')
--- src/app/common/i18n/lang.en.js
+++ src/app/common/i18n/lang.en.js
@@ -1,14 +1,36 @@
 var englishTranslations = {
+
+    /* User auth */
     'LOG_IN': 'Log In',
     'LOG_OUT': 'Log Out',
     'USER_NAME': 'User Name',
     'PASSWORD': 'Password',
     'PROFILE': 'Profile',
 
-    'SEARCH': 'Search',
-
+    /* Projects */
     'PROJECTS': 'Projects',
     'ALL_PROJECTS': 'All Projects',
+
+    /* Tags */
+    'CREATE_NEW_TAG': 'Create new tag',
+
+    /* Other */
+    'SEARCH': 'Search',
+    'UPDATE': 'Update',
+    'TITLE': 'Title',
+    'SCHEMA': 'Schema',
+    'AUTHOR': 'Author',
+    'CREATED': 'Created',
+    'ITEMS_PER_PAGE': 'Items per page',
+
+    /* Dialogs */
+    'CONTENT_MODIFIED': 'Content Modified',
+    'CONTENT_MODIFIED_HOW_TO_PROCEED': 'This content has been modified. How would you like to proceed?',
+    
+    /* Common buttons */
+    'SAVE_CHANGES': 'Save Changes',
+    'DISCARD_CHANGES': 'Discard Changes',
+    'CANCEL': 'Cancel',
 
     /* Error messages */
     'ERROR': 'Error',
