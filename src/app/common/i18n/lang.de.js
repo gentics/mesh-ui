3c10c535e9e2951ab1b09c7439e18aa7a0f7d6bb
Michael Bromley <michael@michaelbromley.co.uk>
2015-04-15 14:53:30 +0200

    Add some translations

--- src/app/common/i18n/lang.de.js
+++ src/app/common/i18n/lang.de.js
@@ -1,6 +1,14 @@
 var germanTranslations = {
-    'LOG_IN': 'Anmelden',
-    'USER_NAME': 'Benutzername'
+    'LOG_IN': 'Einloggen',
+    'LOG_OUT': 'Ausloggen',
+    'USER_NAME': 'Benutzername',
+    'PASSWORD': 'Kennwort',
+    'PROFILE': 'Profil',
+
+    'SEARCH': 'Suche',
+
+    'PROJECTS': 'Projekte',
+    'ALL_PROJECTS': 'Alle Projekte'
 };
 
 angular.module('caiLunAdminUi.common.i18n')
--- src/app/common/i18n/lang.en.js
+++ src/app/common/i18n/lang.en.js
@@ -1,7 +1,18 @@
 var englishTranslations = {
     'LOG_IN': 'Log In',
+    'LOG_OUT': 'Log Out',
     'USER_NAME': 'User Name',
-    'PASSWORD': 'Password'
+    'PASSWORD': 'Password',
+    'PROFILE': 'Profile',
+
+    'SEARCH': 'Search',
+
+    'PROJECTS': 'Projects',
+    'ALL_PROJECTS': 'All Projects',
+
+    /* Error messages */
+    'ERROR': 'Error',
+    'ERR_CHECK_LOGIN_DETAILS': 'Please check your login details and try again.'
 };
 
 angular.module('caiLunAdminUi.common.i18n')
