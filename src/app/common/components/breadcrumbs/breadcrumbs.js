af686e4695e6a85c1cab52a166d07d3ea24eb4e7
Michael Bromley <michael@michaelbromley.co.uk>
2015-04-20 16:22:45 +0200

    Breadcrumbs project link is now navigable.

--- src/app/common/components/breadcrumbs/breadcrumbs.js
+++ src/app/common/components/breadcrumbs/breadcrumbs.js
@@ -4,17 +4,27 @@ angular.module('caiLunAdminUi.common')
 /**
  * Directive to generate breadcrumbs for navigating a project.
  *
+ * @param dataService
  * @param contextService
  * @returns {{}} Directive Definition Object
  */
-function breadcrumbsDirective(contextService) {
+function breadcrumbsDirective(dataService, contextService) {
 
     function breadcrumbsController() {
-        var vm = this;
+        var vm = this,
+            projectName = contextService.getProject().name;
 
-        vm.breadcrumbs = [
-            contextService.getProject().name
-        ];
+        dataService.getProjectRootTagId(projectName)
+            .then(function(id) {
+                vm.breadcrumbs = [
+                    {
+                        name: projectName,
+                        id: id
+                    }
+                ];
+            });
+
+
     }
 
     return {
