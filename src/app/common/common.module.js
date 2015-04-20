b393b19a65329e745a879a67dddafe710443d2b8
Michael Bromley <michael@michaelbromley.co.uk>
2015-04-20 11:31:20 +0200

    Add notifyService to display notification to the user.

--- src/app/common/common.module.js
+++ src/app/common/common.module.js
@@ -1,5 +1,6 @@
 angular.module('caiLunAdminUi.common', [
     'caiLunAdminUi.common.i18n',
     'restangular',
-    'ngCookies'
+    'ngCookies',
+    'ngMaterial'
 ]);
---
+++ src/app/common/services/notifyService.js
@@ -0,0 +1,39 @@
+angular.module('caiLunAdminUi.common')
+    .service('notifyService', NotifyService);
+
+/**
+ * Service that deals with notifications for the user. There are two types of
+ * notification: toast and message.
+ *
+ * Toast is for transient status updates that are displayed for a short time to the
+ * user upon completion of some action, e.g. saving changes.
+ *
+ * Messages are for more persistent notifications that require user-intervention to
+ * view and dismiss. The presence of such messages would be indicated by some
+ * visual alert in the UI, which the user can then click through to get to
+ * a list of current messages.
+ *
+ * @param $mdToast
+ * @param i18n
+ * @constructor
+ */
+function NotifyService($mdToast, i18n) {
+
+    this.toast = toast;
+    this.message = message;
+
+    /**
+     * Display a toast (popup) message containing the specified text.
+     * @param text
+     */
+    function toast(text) {
+        var popup = $mdToast.simple()
+            .content(i18n(text))
+            .position('top right');
+        $mdToast.show(popup);
+    }
+
+    function message(text) {
+        // TODO: implement
+    }
+}
