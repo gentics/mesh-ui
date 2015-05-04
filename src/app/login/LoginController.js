69336b1a2caa281412ce497d7cca74856a1ae057
Michael Bromley <michael@michaelbromley.co.uk>
2015-05-04 16:04:28 +0200

    Create custom button style and replace material design ones.

--- src/app/login/LoginController.js
+++ src/app/login/LoginController.js
@@ -28,14 +28,13 @@ function LoginController($timeout, $state, $mdDialog, i18n, authService) {
      * Displays an error message on login failure.
      * @param event
      */
-    function showErrorDialog(event) {
-        var logInErrorDialog = $mdDialog.alert()
-            .title(i18n('ERROR'))
-            .content(i18n('ERR_CHECK_LOGIN_DETAILS'))
-            .ariaLabel(i18n('ERR_CHECK_LOGIN_DETAILS'))
-            .ok('Okay')
-            .targetEvent(event);
-
-        $mdDialog.show(logInErrorDialog);
+    function showErrorDialog() {
+        return $mdDialog.show({
+            templateUrl: 'login/loginErrorDialog.html',
+            controller: function($mdDialog) {
+                this.cancel = function() { $mdDialog.cancel(); };
+            },
+            controllerAs: 'vm'
+        });
     }
 }
