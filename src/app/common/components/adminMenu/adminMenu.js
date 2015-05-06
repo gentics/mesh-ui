c2e4a551b9ca5c1657eb687ff1457a96e920f87b
Michael Bromley <michael@michaelbromley.co.uk>
2015-05-06 17:29:33 +0100

    Refactor & document adminMenu component

--- src/app/common/components/adminMenu/adminMenu.js
+++ src/app/common/components/adminMenu/adminMenu.js
@@ -3,7 +3,12 @@ angular.module('caiLunAdminUi.common')
     .directive('adminMenuButton', adminMenuButtonDirective)
     .factory('adminMenuService', adminMenuService);
 
-
+/**
+ * Admin menu component.
+ *
+ * @param adminMenuService
+ * @returns {{restrict: string, templateUrl: string, controller: adminMenuController, controllerAs: string, scope: {}, replace: boolean}}
+ */
 function adminMenuDirective(adminMenuService) {
 
     function adminMenuController() {
@@ -21,6 +26,12 @@ function adminMenuDirective(adminMenuService) {
     };
 }
 
+/**
+ * Button which toggles the admin menu open/closed.
+ *
+ * @param adminMenuService
+ * @returns {{restrict: string, controller: adminMenuButtonController, controllerAs: string, template: string, scope: {}}}
+ */
 function adminMenuButtonDirective(adminMenuService) {
 
     function adminMenuButtonController() {
@@ -37,7 +48,17 @@ function adminMenuButtonDirective(adminMenuService) {
     };
 }
 
-function adminMenuService($rootScope, $document) {
+/**
+ * This service provides communication between the admin menu button and the menu itself. It holds the open/closed
+ * state of the menu, and performs related actions when that state changes to facilitate the working of the
+ * component.
+ *
+ * @param {ng.IRootScopeService} $rootScope
+ * @param {ng.IDocumentService} $document
+ * @param {ng.ui.IStateService} $state
+ * @returns {{isOpen: isOpen, toggle: toggle}}
+ */
+function adminMenuService($rootScope, $document, $state, contextService) {
     var _isOpen = false;
 
     return {
@@ -45,25 +66,62 @@ function adminMenuService($rootScope, $document) {
         toggle: toggle
     };
 
+    /**
+     * Is the admin menu currently open?
+     *
+     * @returns {boolean}
+     */
     function isOpen() {
         return _isOpen;
     }
 
+    /**
+     * Toggle the open/closed state of the admin menu. Also registers/de-registers a
+     * document-level click handler to cause auto-hide when not in one of the
+     * admin states.
+     */
     function toggle() {
         _isOpen = !_isOpen;
+
         if (_isOpen) {
             $document.on('click', closeAdminMenu);
+        } else {
+            $document.off('click', closeAdminMenu);
+            if (isAdminState()) {
+                var projectName = contextService.getProject().name,
+                    tagId = contextService.getTag().id;
+
+                if (projectName) {
+                    $state.go('projects.explorer', {projectName: projectName, tagId: tagId});
+                } else {
+                    $state.go('projects.list');
+                }
+            }
         }
     }
 
+    /**
+     * Handler to provide auto-hide when clicking off the menu when not in an admin state.
+     * @param event
+     */
     function closeAdminMenu(event) {
         var target = event.target,
             appViewport = document.querySelector('.app-viewport');
 
-        if (target === appViewport || appViewport.contains(target)) {
+        if (!isAdminState() && (target === appViewport || appViewport.contains(target))) {
             $rootScope.$apply(function() {
-                _isOpen = false;
+                toggle();
             });
         }
+    }
+
+    /**
+     * Returns true is the current router state is one of the "admin" states as defined in
+     * the admin.config.js state definitions.
+     *
+     * @returns {boolean}
+     */
+    function isAdminState() {
+        return !!($state.current.data && $state.current.data.isAdminState === true);
     }
 }
