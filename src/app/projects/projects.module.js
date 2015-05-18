0eb1b62e370ff1ee7d9f639197f1c892fdfc32d1
Michael Bromley <michael@michaelbromley.co.uk>
2015-05-18 14:58:05 +0200

    Tidy up

--- src/app/projects/projects.module.js
+++ src/app/projects/projects.module.js
@@ -1,3 +1,4 @@
+// TODO: rename the whole "projects" section to something like "explorer" to reflect the logical separation between explorer/admin areas
 angular.module('meshAdminUi.projects', [
     'meshAdminUi.common',
     'ui.router'
