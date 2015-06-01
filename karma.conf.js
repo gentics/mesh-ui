f69ca8722fafb4cf70d214a1f8fb24f1ac66311c
Michael Bromley <michael@michaelbromley.co.uk>
2015-06-01 15:25:18 +0200

    Report Karma test results with jUnit reporter

--- karma.conf.js
+++ karma.conf.js
@@ -42,7 +42,11 @@ module.exports = function(config) {
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['progress'],
+        reporters: ['progress', 'junit'],
+
+        junitReporter: {
+            outputFile: 'build/junit.xml'
+        },
 
 
         // web server port
