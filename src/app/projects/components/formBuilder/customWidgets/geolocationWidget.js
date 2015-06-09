11a0928c8c618f66ef7866355c6e1324731d57bb
Michael Bromley <michael@michaelbromley.co.uk>
2015-06-09 15:13:24 +0200

    Fix bug in geolocation widget when loading multiple maps

--- src/app/projects/components/formBuilder/customWidgets/geolocationWidget.js
+++ src/app/projects/components/formBuilder/customWidgets/geolocationWidget.js
@@ -17,6 +17,7 @@ function geolocationWidgetDirective() {
         prependScriptNode(element, initMap);
 
         function initMap() {
+            delete window._googleMapsIsLoading;
             var mapCanvas = element[0].querySelector('.map-container');
             var mapOptions = {
                 center: { lat: parseFloat(scope.microschemaModel.latitude), lng: parseFloat(scope.microschemaModel.longitude)},
@@ -38,14 +39,22 @@ function geolocationWidgetDirective() {
     }
 
     function prependScriptNode(element, callback) {
-        if (!window.google || !window.google.maps) {
+        if ((window.google && window.google.maps) || window._googleMapsIsLoading) {
+            // hacky polling to see if the Google Maps script has loaded and initialized yet.
+            (function checkLoaded() {
+                if (window._googleMapsIsLoading) {
+                    setTimeout(checkLoaded, 50);
+                } else {
+                    callback();
+                }
+            })();
+        } else {
             window.geolocationWidgetCallback = callback;
+            window._googleMapsIsLoading = true;
             var mapScriptNode = document.createElement('script');
             mapScriptNode.src = "https://maps.googleapis.com/maps/api/js?v=3.exp&callback=geolocationWidgetCallback&key=" + GOOGLE_MAPS_API_KEY;
             mapScriptNode.type = 'text/javascript';
             element.prepend(mapScriptNode);
-        } else {
-            callback();
         }
     }
 
