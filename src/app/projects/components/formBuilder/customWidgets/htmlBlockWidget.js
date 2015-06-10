e40ba9b3db863c343721f42be008868c7e57044a
Michael Bromley <michael@michaelbromley.co.uk>
2015-06-10 08:52:13 +0200

    Create custom htmlBlockWidget

---
+++ src/app/projects/components/formBuilder/customWidgets/htmlBlockWidget.js
@@ -0,0 +1,17 @@
+angular.module('meshAdminUi.projects.formBuilder')
+    .directive('htmlBlockWidget', htmlBlockWidgetDirective);
+
+/**
+ * Custom input widget for geolocation microschema. Just a demo.
+ *
+ * @returns {ng.IDirective} Directive definition object
+ */
+function htmlBlockWidgetDirective() {
+
+    return {
+        restrict: 'E',
+        replace: true,
+        template: '<div><div medium-editor class="htmlField" ng-model="microschemaModel.content" ng-change="formBuilder.modified = true" ng-readonly="!formBuilder.canUpdate()"></div></div>',
+        scope: true
+    };
+}
