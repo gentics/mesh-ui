40db2fb0bec98d2fd88e66ef46e1f4c126f62ecf
Michael Bromley <michael@michaelbromley.co.uk>
2015-06-23 10:15:58 +0200

    Get htmlBlockWidget working with Aloha Editor

--- src/app/projects/components/formBuilder/customWidgets/htmlBlockWidget.js
+++ src/app/projects/components/formBuilder/customWidgets/htmlBlockWidget.js
@@ -8,11 +8,17 @@ angular.module('meshAdminUi.projects.formBuilder')
  */
 function htmlBlockWidgetDirective() {
 
+    function htmlBlockWidgetLinkFn(scope) {
+        scope.model = scope.microschemaModel;
+        scope.path = scope.microschemaFields[0].name;
+    }
+
     return {
         restrict: 'E',
         replace: true,
+        link: htmlBlockWidgetLinkFn,
         template: '<div class="html-container editor-widget">' +
-                    '<div medium-editor class="htmlField" ng-model="microschemaModel.content" ng-change="formBuilder.modified = true" ng-readonly="!formBuilder.canUpdate()"></div>' +
+                    '<mh-html-widget></mh-html-widget>' +
                   '</div>',
         scope: true
     };
