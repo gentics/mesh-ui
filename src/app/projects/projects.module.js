f5f775c06050e54d4cedd74a00569f9c18648dd2
Michael Bromley <michael@michaelbromley.co.uk>
2015-07-07 16:44:53 +0200

    Tidy up some styles

--- src/app/projects/projects.module.js
+++ src/app/projects/projects.module.js
@@ -1,4 +1,4 @@
-// TODO: rename the whole "projects" section to something like "explorer" to reflect the logical separation between explorer/admin areas
+// TODO: rename the whole "projects" section to something like "editor" to reflect the logical separation between explorer/admin areas
 angular.module('meshAdminUi.projects', [
     'meshAdminUi.projects.formBuilder',
     'meshAdminUi.common',
