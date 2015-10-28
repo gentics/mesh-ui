a9a5722d11c1826756d8d94a46d10cd68bd9c202
Michael Bromley <michael@michaelbromley.co.uk>
2015-10-28 10:55:12 +0100

    Improve performance of resizer component

--- src/app/projects/components/resizer/resizer.ts
+++ src/app/projects/components/resizer/resizer.ts
@@ -7,10 +7,13 @@ module meshAdminUi {
             'max-width' : '50%',
             'min-width' : '50%'
         };
-        private resizerMouseIsDown = false;
+        private barWidth = 10;
+        private percentage: number;
+        public resizerMouseIsDown = false;
+        public handleStyle: any = {};
 
 
-        constructor(private $scope: ng.IScope, $element: ng.IAugmentedJQuery) {
+        constructor(private $scope: ng.IScope, private $element: ng.IAugmentedJQuery) {
             const mouseUpHandler = (event) => {
                 this.resizerMouseup(event);
             };
@@ -24,39 +27,51 @@ module meshAdminUi {
             $scope.$on('$destroy', () => {
                 $element.off('mouseup', mouseUpHandler);
                 $element.off('mousemove', mouseMoveHandler);
-            })
+            });
         }
 
-        public resizerMousedown(event) {
+        public resizerMousedown(event: Event) {
             this.resizerMouseIsDown = true;
             event.preventDefault();
         }
 
         public resizerMouseup(event) {
             this.resizerMouseIsDown = false;
+            this.$element.css({ position: 'relative' });
+            this.$scope.$apply(() => {
+                this.resizerStyle = {
+                    'max-width': this.percentage + '%',
+                    'min-width': this.percentage + '%'
+                };
+            });
         }
 
-        public resizerMousemove(event: any) {
+        public resizerMousemove(event: any): number {
             if (this.resizerMouseIsDown) {
                 let minWidth = 20,
                     maxWidth = 80,
-                    barWidth = 10,
                     mouseLeftOffset = event.clientX,
                     containerWidth = event.currentTarget.offsetWidth,
-                    mouseContainerOffset = mouseLeftOffset - event.currentTarget.offsetLeft - barWidth,
-                    percentage = Math.max(Math.min(mouseContainerOffset / containerWidth * 100, maxWidth), minWidth);
+                    mouseContainerOffset = mouseLeftOffset - this.getContainerOffset(),
+                    percentage = mouseContainerOffset / containerWidth * 100;
 
-                this.$scope.$apply(() => {
-                    this.resizerStyle = {
-                        'max-width': percentage + '%',
-                        'min-width': percentage + '%'
-                    };
-                });
+                this.percentage = Math.max(Math.min(percentage, maxWidth), minWidth);
+
                 // if the mouse has moved outside of the container, trigger the mouseup event.
                 if (event.clientY < event.currentTarget.getBoundingClientRect().top) {
                     this.resizerMouseup(event);
                 }
+
+                if (minWidth < percentage && percentage < maxWidth) {
+                    this.$scope.$apply(() => this.handleStyle.left = mouseContainerOffset + 'px');
+                }
+                event.preventDefault();
+                return mouseContainerOffset;
             }
+        }
+
+        public getContainerOffset() {
+            return this.$element[0].offsetLeft + this.barWidth / 2;
         }
 
     }
@@ -71,9 +86,15 @@ module meshAdminUi {
             scope: {
             },
             link: (scope, element, attrs, ctrl: ResizerController) => {
+
+                scope.ctrl = ctrl;
+
                 scope.mouseDown = (event) => {
+                    let resizeBar = <HTMLElement>document.querySelector('.resize-bar');
+                    ctrl.handleStyle.height = resizeBar.getBoundingClientRect().height + 'px';
+                    ctrl.handleStyle.left = (resizeBar.getBoundingClientRect().left - ctrl.getContainerOffset())+ 'px';
                     ctrl.resizerMousedown(event);
-                }
+                };
             }
         };
     }
