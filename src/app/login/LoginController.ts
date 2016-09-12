9436369f4e99dc94e539db3da648cfd2b40768fe
Michael Bromley <apagtxbromley@apa.at>
2016-09-12 11:29:18 +0200

    Fix https://jira.gentics.com/browse/CL-484

--- src/app/login/LoginController.ts
+++ src/app/login/LoginController.ts
@@ -13,7 +13,12 @@ module meshAdminUi {
 
         public submitForm(event: Event, userName: string, password: string) {
             this.loggingIn = true;
-            this.authService.logIn(userName, password)
+            // The user may already be "logged in" as a public user from viewing the front-end
+            // (if that is on the same domain as the admin app), so we must explicitly log out
+            // to ensure the user is not then logged in as the public uer.
+            this.authService.logOut()
+                .catch(err => {})
+                .then(() => this.authService.logIn(userName, password))
                 .then(() => {
                     this.success = true;
                     this.$timeout(() => this.$state.go('projectsList'), 1500);
