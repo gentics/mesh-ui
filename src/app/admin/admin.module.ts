2852d073d62d00403a66239317a2d53d4c258732
Philipp Gürtler <pg@gentics.com>
2017-05-05 08:45:16 +0200

    Remove Project Detail

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -5,15 +5,13 @@ import { routes } from './admin.routes';
 import { SharedModule } from '../shared/shared.module';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ProjectListComponent } from './components/project/project-list/project-list.component';
-import { ProjectDetailComponent } from './components/project/project-detail/project-detail.component';
 import { ProjectListItemComponent } from './components/project/project-list/components/project-list-item/project-list-item.component';
 
 @NgModule({
     declarations: [
         DashboardComponent,
         ProjectListComponent,
-        ProjectListItemComponent,
-        ProjectDetailComponent
+        ProjectListItemComponent
     ],
     imports: [
         SharedModule,
--- src/app/admin/admin.routes.ts
+++ src/app/admin/admin.routes.ts
@@ -1,9 +1,7 @@
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ProjectListComponent } from './components/project/project-list/project-list.component';
-import { ProjectDetailComponent } from './components/project/project-detail/project-detail.component';
 
 export const routes = [
     { path: '', component: DashboardComponent },
-    { path: 'projects', component: ProjectListComponent },
-    { path: 'projects/:uuid', component: ProjectDetailComponent }
+    { path: 'projects', component: ProjectListComponent }
 ];
