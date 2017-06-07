a0bed41ad86365da21d0993912c365b5863be408
Leon Adler <leonadler@gentics.com>
2017-06-07 14:50:15 +0200

    Don't declare all modules as "any"

--- src/custom-typings.d.ts
+++ src/custom-typings.d.ts
@@ -44,7 +44,7 @@ import * as _ from 'lodash'
  */
 
 // support NodeJS modules without type definitions
-declare module '*';
+declare module '*.ngfactory';
 
 /*
 // for legacy tslint etc to understand rename 'modern-lru' with your package
