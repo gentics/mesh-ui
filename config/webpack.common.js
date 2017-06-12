995e1085d62817269c5cad76131732d3f44f2990
Michael Bromley <m.bromley@gentics.com>
2017-06-12 08:47:39 +0200

    Update page title

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -24,7 +24,7 @@ const ngcWebpack = require('ngc-webpack');
 const HMR = helpers.hasProcessFlag('hot');
 const AOT = helpers.hasNpmFlag('aot');
 const METADATA = {
-    title: 'Angular2 Webpack Starter by @gdi2290 from @AngularClass',
+    title: 'Gentics Mesh',
     baseUrl: '/',
     isDevServer: helpers.isWebpackDevServer()
 };
