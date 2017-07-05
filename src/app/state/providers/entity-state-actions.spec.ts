f4c90f1d3681077e66a4c60c444c2033f698b559
Leon Adler <leonadler@gentics.com>
2017-07-05 11:27:31 +0200

    Add entity state merging
    Most work is done in immutablets 0.11 - When we load an entity from the server twice, the entity object remains the same reference (before === after).

---
+++ src/app/state/providers/entity-state-actions.spec.ts
@@ -0,0 +1,156 @@
+import { mergeEntityState } from './entity-state-actions';
+import { EntityState } from '../models/entity-state.model';
+import { User } from '../../common/models/user.model';
+
+fdescribe('EntityStateActions', () => {
+
+    describe('mergeEntityState()', () => {
+
+        const empty: EntityState = {
+            node: {},
+            project: {},
+            schema: {},
+            user: {}
+        };
+
+        it('adds new entities to the output hash', () => {
+            const before = empty;
+            const after = mergeEntityState(before, {
+                user: {
+                    'admin-uuid': {
+                        uuid: 'admin-uuid',
+                        username: 'admin'
+                    }
+                }
+            });
+
+            expect(after).not.toBe(before);
+            expect(after).not.toEqual(before);
+            expect(after.user).toEqual({
+                'admin-uuid': {
+                    uuid: 'admin-uuid',
+                    username: 'admin'
+                } as Partial<User> as User
+            });
+        });
+
+        it('does not change the old state', () => {
+            const before = empty;
+            const after = mergeEntityState(before, {
+                user: {
+                    'admin-uuid': {
+                        uuid: 'admin-uuid',
+                        username: 'admin'
+                    }
+                }
+            });
+
+            expect(before).toEqual({
+                node: {},
+                project: {},
+                schema: {},
+                user: {}
+            });
+        });
+
+        it('reuses references of unchanged objects', () => {
+            const before = {
+                ...empty,
+                user: {
+                    'admin-uuid': {
+                        uuid: 'admin-uuid',
+                        username: 'admin'
+                    } as User
+                }
+            };
+            const after = mergeEntityState(before, {
+                user: {
+                    'admin-uuid': {
+                        uuid: 'admin-uuid',
+                        username: 'admin'
+                    }
+                }
+            });
+
+            expect(after.user).toBe(before.user);
+        });
+
+        describe('reusing object references', () => {
+
+            let before: EntityState;
+            let after: EntityState;
+
+            beforeAll(() => {
+                before = {
+                    ...empty,
+                    user: {
+                        'admin-uuid': {
+                            uuid: 'admin-uuid',
+                            username: 'admin',
+                            groups: [
+                                {
+                                    name: 'firstGroup',
+                                    uuid: 'first-group-uuid'
+                                }
+                            ]
+                        } as User
+                    }
+                };
+                after = mergeEntityState(before, {
+                    user: {
+                        'admin-uuid': {
+                            uuid: 'admin-uuid',
+                            username: 'admin',
+                            groups: [
+                                {
+                                    name: 'firstGroup',
+                                    uuid: 'first-group-uuid'
+                                },
+                                {
+                                    name: 'secondGroup',
+                                    uuid: 'second-group-uuid'
+                                }
+                            ]
+                        }
+                    }
+                });
+            });
+
+            it('creates new objects of changed object properties', () => {
+                expect(after).not.toBe(before);
+                expect(after.user).not.toBe(before.user);
+                expect(after.user['admin-uuid']).not.toBe(before.user['admin-uuid']);
+                expect(before.user['admin-uuid'].groups).toEqual([
+                    {
+                        name: 'firstGroup',
+                        uuid: 'first-group-uuid'
+                    }
+                ]);
+                expect(after.user['admin-uuid'].groups).toEqual([
+                    {
+                        name: 'firstGroup',
+                        uuid: 'first-group-uuid'
+                    },
+                    {
+                        name: 'secondGroup',
+                        uuid: 'second-group-uuid'
+                    }
+                ]);
+            });
+
+            it('reuses references of unchanged object properties', () => {
+                const isSameReference = before.user['admin-uuid'].groups[0] === after.user['admin-uuid'].groups[0];
+                expect(isSameReference).toBe(true);
+            });
+
+            it('keeps entity properties not provided in the changes object', () => {
+                // { a: 5, b: 7 } plus { a: 5 } should be the first reference
+                const isSameReference = before.user['admin-uuid'].groups[0] === after.user['admin-uuid'].groups[0];
+                expect(isSameReference).toBe(true);
+            });
+
+        });
+
+    });
+
+});
--- src/app/state/providers/entity-state-actions.ts
+++ src/app/state/providers/entity-state-actions.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Immutable, StateActionBranch } from 'immutablets';
+import { deepApplyWithReuse, Immutable, StateActionBranch } from 'immutablets';
 import { EntityState } from '../models/entity-state.model';
 import { AppState } from '../models/app-state.model';
 
@@ -21,4 +21,69 @@ export class EntityStateActions extends StateActionBranch<AppState> {
             }
         });
     }
+}
+
+
+type EntityBranchChanges<K extends keyof EntityState> = { [uuid: string]: Partial<EntityState[K][string]> } | Array<Partial<EntityState[K][string]>>;
+
+/**
+ * Utility function to update the entity state.
+ *
+ * Returns a new entity state with the passed changes applied.
+ * Reuses references of objects whenever possible.
+ * Arrays are not merged, and only reuse references of their elements when they are equal.
+ */
+export function mergeEntityState(oldState: EntityState, changes: {[K in keyof EntityState]?: EntityBranchChanges<K>}): EntityState {
+    const newState = Object.assign({}, oldState);
+    let anyBranchChanged = false;
+
+    for (const key of Object.keys(changes)) {
+        if (!oldState[key]) {
+            throw new Error(`mergeEntityState: Trying to merge nonexisting entity key "${key}"`);
+        }
+        const branch = mergeEntityStateBranch(oldState[key], changes[key]);
+        if (branch !== oldState[key]) {
+            newState[key] = branch;
+            anyBranchChanged = true;
+        }
+    }
+
+    return anyBranchChanged ? newState : oldState;
+}
+
+/**
+ * Merge an entity branch with a set of changes.
+ * Reuses references of unchanged objects.
+ */
+function mergeEntityStateBranch<B extends keyof EntityState>(oldBranch: EntityState[B], changes: EntityBranchChanges<B>): EntityState[B] {
+    const newBranch = Object.assign({}, oldBranch);
+    let anyEntityChanged = false;
+
+    // Changes can be an array or an object.
+    if (Array.isArray(changes)) {
+        for (const change of changes) {
+            const uuid: string = change && (change as any).uuid;
+            if (!uuid) {
+                throw new Error(`mergeEntityState: Provided value has no uuid. ` + JSON.stringify(change, null, 4));
+            }
+
+            // const entity = mergeEntity(oldBranch[uuid], change);
+            const entity = deepApplyWithReuse(oldBranch[uuid], change);
+            if (entity !== oldBranch[uuid]) {
+                oldBranch[uuid] = entity;
+                anyEntityChanged = true;
+            }
+        }
+    } else {
+        for (const uuid of Object.keys(changes)) {
+            // const entity = mergeEntity(oldBranch[uuid], changes[uuid] as any);
+            const entity = deepApplyWithReuse(oldBranch[uuid], changes[uuid]);
+            if (entity !== oldBranch[uuid]) {
+                newBranch[uuid] = entity;
+                anyEntityChanged = true;
+            }
+        }
+    }
+
+    return anyEntityChanged ? newBranch : oldBranch;
 }
