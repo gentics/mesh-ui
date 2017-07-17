2d238cb15e6a4fc047497c309f6b9a0e858b519f
Michael Bromley <m.bromley@gentics.com>
2017-07-17 14:45:57 +0200

    Fix navigating to a default project on MasterDetailComponent init

--- src/app/editor/components/master-detail/master-detail.component.ts
+++ src/app/editor/components/master-detail/master-detail.component.ts
@@ -3,6 +3,7 @@ import { ApplicationStateService } from '../../../state/providers/application-st
 import { Observable } from 'rxjs/Observable';
 import { NavigationService } from '../../../core/providers/navigation/navigation.service';
 import { Project } from '../../../common/models/project.model';
+import { ListEffectsService } from '../../../core/providers/effects/list-effects.service';
 
 @Component({
     selector: 'master-detail',
@@ -15,12 +16,15 @@ export class MasterDetailComponent implements OnInit {
     editorOpen$: Observable<boolean>;
 
     constructor(private state: ApplicationStateService,
+                private listEffects: ListEffectsService,
                 private navigationService: NavigationService) {
         this.editorFocused$ = state.select(state => state.editor.editorIsFocused);
         this.editorOpen$ = state.select(state => state.editor.editorIsOpen);
     }
 
     ngOnInit(): void {
+        this.listEffects.loadProjects();
+
         // TODO: We need to determine a "default" project to load up on init, fetch it from the
         // API and navigate to its baseNode.
         // this.navigationService.list('demo', 'container_uuid').navigate();
@@ -34,7 +38,7 @@ export class MasterDetailComponent implements OnInit {
             .filter(Boolean)
             .take(1)
             .subscribe((firstProject: Project) => {
-                this.navigationService.list(firstProject.name, firstProject.rootNode.uuid);
+                this.navigationService.list(firstProject.name, firstProject.rootNode.uuid).navigate();
             });
     }
 
