bb06445aab08e1bf0ae2f3dfca4bde1092ba5393
Michael Bromley <m.bromley@gentics.com>
2017-08-01 11:12:36 +0200

    Create NodeLanguageLabelComponent

--- src/app/editor/editor.module.ts
+++ src/app/editor/editor.module.ts
@@ -1,19 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ModalService, OverlayHostService } from 'gentics-ui-core';
 
-import { routes } from './editor.routes';
-import { SharedModule } from '../shared/shared.module';
-import { MasterDetailComponent } from './components/master-detail/master-detail.component';
-import { FormGeneratorModule } from './form-generator/form-generator.module';
-import { NodeEditorComponent } from './components/node-editor/node-editor.component';
 import { BreadcrumbsComponent } from './components/breadcrumbs/breadcrumbs.component';
-import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { ConfirmNavigationModalComponent } from './components/confirm-navigation-modal/confirm-navigation-modal.component';
 import { ContainerContentsComponent } from './components/container-contents/container-contents.component';
 import { EditorEffectsService } from './providers/editor-effects.service';
-import { ProjectSwitcherComponent } from './components/project-switcher/project-switcher.component';
+import { FormGeneratorModule } from './form-generator/form-generator.module';
+import { MasterDetailComponent } from './components/master-detail/master-detail.component';
+import { NodeEditorComponent } from './components/node-editor/node-editor.component';
 import { NodeEditorGuard } from './providers/node-editor-guard';
-import { ConfirmNavigationModalComponent } from './components/confirm-navigation-modal/confirm-navigation-modal.component';
-import { ModalService, OverlayHostService } from 'gentics-ui-core';
+import { NodeLanguageLabelComponent } from './components/node-language-label/node-language-label.component';
+import { ProjectSwitcherComponent } from './components/project-switcher/project-switcher.component';
+import { routes } from './editor.routes';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
     imports: [
@@ -22,13 +23,14 @@ import { ModalService, OverlayHostService } from 'gentics-ui-core';
         FormGeneratorModule
     ],
     declarations: [
+        BreadcrumbsComponent,
         ConfirmNavigationModalComponent,
         ContainerContentsComponent,
         MasterDetailComponent,
         NodeEditorComponent,
-        BreadcrumbsComponent,
-        SearchBarComponent,
-        ProjectSwitcherComponent
+        NodeLanguageLabelComponent,
+        ProjectSwitcherComponent,
+        SearchBarComponent
     ],
     entryComponents: [
         ConfirmNavigationModalComponent
