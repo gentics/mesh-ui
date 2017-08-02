09cf4028b64017b39b420cfc4004d0fb2f29796b
Michael Bromley <m.bromley@gentics.com>
2017-08-02 18:44:26 +0200

    Create EntitySelectService

--- src/app/state/state.module.ts
+++ src/app/state/state.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { ApplicationStateService } from './providers/application-state.service';
 import { ApplicationStateDevtools } from './providers/application-state-devtools';
+import { EntitySelectService } from './providers/entity-select.service';
 
 @NgModule({
-    providers: [ApplicationStateService, ApplicationStateDevtools],
+    providers: [ApplicationStateService, EntitySelectService, ApplicationStateDevtools],
 })
 export class StateModule {
 }
