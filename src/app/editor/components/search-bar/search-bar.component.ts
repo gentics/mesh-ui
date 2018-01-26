b6bf886d2a9150e47096b35c4c3d3da0ecddbe40
Linas Ozeraitis <L.Ozeraitis@gentics.com>
2018-01-26 13:42:42 +0100

    initial search-bar mock

--- src/app/editor/components/search-bar/search-bar.component.ts
+++ src/app/editor/components/search-bar/search-bar.component.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import { ChangeDetectorRef } from '@angular/core';
+import { ApiService } from '../../../core/providers/api/api.service';
 
 @Component({
     selector: 'search-bar',
     templateUrl: './search-bar.component.html',
-    styleUrls: ['./search-bar.scss']
+    styleUrls: ['./search-bar.scss'],
 })
 
 export class SearchBarComponent {
+    protected searching = false;
+    protected filtersOpen = false;
 
+    constructor(
+        private changeDetectorRef: ChangeDetectorRef,
+        private api: ApiService
+    ) {}
+
+    search(fraze: String) {
+
+        this.searching = true;
+
+        setTimeout(() => {
+            this.searching = false;
+            this.changeDetectorRef.detectChanges();
+        }, 1000);
+    }
+
+    openFilters() {
+        this.filtersOpen = true;
+    }
+
+    closeFilters() {
+        this.filtersOpen = false;
+    }
 }
