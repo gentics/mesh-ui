7a377c4cc1c8a5074612d92d3527234cd46cd1b5
Linas Ozeraitis <L.Ozeraitis@gentics.com>
2018-03-07 09:45:20 +0100

    add highlight pipe, it will eventualy replace the FilterSelection

--- src/app/common/util/fuzzy-search.ts
+++ src/app/common/util/fuzzy-search.ts
@@ -1,19 +1,23 @@
 import { FilterSelection } from "../models/common.model";
 
-function escapeRegExp(text: string): string {
-    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
+export function fuzzyEscapeRegExp(text: string): string {
+    return text.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+    //return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
 }
 
-export function fuzzySearch(needle: string, haystack: string[]): string[] {
-    const regexParts = needle.split('').map(escapeRegExp);
-    const regex = new RegExp(regexParts.join('.*?'), 'i');
-    return haystack.filter(item => regex.test(item));
-}
+export function getFuzzyRegExp(term): RegExp {
+    /*
+    // The following would match words with non-matching symbols in between
+    // e.g 'autos' would match 'AUTOmobileS'
+        const regexParts = needle.split('').map(fuzzyEscapeRegExp);
+        const regex = new RegExp(regexParts.join('.*?'), 'i');
+    */
 
+    return new RegExp(`(${fuzzyEscapeRegExp(term)})`, 'gi');
+}
 
 export function fuzzyMatch(needle: string, haystack: string): string[] {
-    const regexParts = needle.split('').map(escapeRegExp);
-    let regex = new RegExp(regexParts.join('.*?'), 'i');
+    const regex = getFuzzyRegExp(needle);
     return regex.exec(haystack);
 }
 
@@ -21,7 +25,10 @@ export function fuzzyMatch(needle: string, haystack: string): string[] {
 export function fuzzyReplace(needle: string, haystack: string): FilterSelection {
     const matches = fuzzyMatch(needle, haystack);
     if (matches && matches.length) {
-        return { value: haystack, valueFormatted: haystack.replace(matches[0], `<span class="filterSelection">${matches[0]}</span>`)};
+        return {
+            value: haystack,
+            valueFormatted: haystack.replace(matches[0], `<span class="filter-selection">${matches[0]}</span>`),
+        };
     } else {
         return null;
     }
