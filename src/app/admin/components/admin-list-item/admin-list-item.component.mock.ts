372064febc0e1abf18390ecd8009e1988d2a3c9e
Iepikhin Oleksii <apagtxiepikhin@lan.apa.at>
2018-07-26 13:20:49 +0200

    Fix all the e2e tests for Schema/Microschema, begin with unit tests for Microschema.

--- src/app/admin/components/admin-list-item/admin-list-item.component.mock.ts
+++ src/app/admin/components/admin-list-item/admin-list-item.component.mock.ts
@@ -1,9 +1,11 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
     selector: 'mesh-admin-list-item',
     template: ``
 })
+
+// tslint:disable-next-line:component-class-suffix
 export class MockAdminListItem {
     @Input() checked = false;
     @Input() selectable = true;
