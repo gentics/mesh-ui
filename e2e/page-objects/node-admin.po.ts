b05dbe8ff75a37e5266755b756e8127f2a0caeba
Philip <Philip.Schulz-Klingauf@gentics.com>
2018-12-13 16:32:09 +0100

    undid nav bar changes

--- e2e/page-objects/node-admin.po.ts
+++ e2e/page-objects/node-admin.po.ts
@@ -1,22 +1,15 @@
-import { browser, by, element, ElementFinder } from 'protractor';
+import { browser, by, element } from 'protractor';
 
 export class NodeAdmin {
-    // private readonly mode = element(by.css('.gtx-top-bar-left')).element(by.css('.button-event-wrapper'));
-    private readonly appNavigator = element(by.css('.gtx-top-bar-left')).element(by.css('.nav-icon'));
-    private appIcon: ElementFinder;
+    private readonly mode = element(by.css('.gtx-top-bar-left')).element(by.css('.button-event-wrapper'));
 
     async modeChange() {
-        await this.appNavigator.click();
-        this.appIcon = await element(by.css('.gtx-top-bar-left')).element(by.css('.app-icon'));
-        await this.appIcon.click();
+        await this.mode.click();
         await browser.waitForAngular();
     }
 
-    async getAppName() {
-        await this.appNavigator.click();
-        return await element(by.css('.gtx-top-bar-left'))
-            .element(by.css('.app-icon'))
-            .getAttribute('data-app');
+    getModeText() {
+        return this.mode.getText();
     }
 
     getBreadcrumbs() {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,46 +14,11 @@ import { ApplicationStateService } from './state/providers/application-state.ser
 export class AppComponent {
     loggedIn$: Observable<boolean>;
     adminMode$: Observable<boolean>;
-    appList$: Observable<any>;
 
     constructor(public state: ApplicationStateService, devtools: ApplicationStateDevtools, private router: Router) {
         this.loggedIn$ = state.select(_state => _state.auth.loggedIn);
         this.adminMode$ = this.router.events
-            .filter((event: NavigationEnd) => event instanceof NavigationEnd)
+            .filter(event => event instanceof NavigationEnd)
             .map((event: NavigationEnd) => /^\/admin/.test(event.url));
-
-        // configure app navigator list
-        this.appList$ = this.adminMode$.map((adminMode: boolean) => {
-            if (adminMode) {
-                return [
-                    {
-                        name: 'editor',
-                        label: 'CMS',
-                        icon: 'edit'
-                    }
-                ];
-            } else {
-                return [
-                    {
-                        name: 'admin',
-                        label: 'Administration',
-                        icon: 'vpn_key'
-                    }
-                ];
-            }
-        });
-    }
-
-    /**
-     * On click on app navigator app icon
-     * @param app identifier string for app route
-     */
-    navigateToApp(app: string) {
-        const routerLink: any[] = [`/${app}`];
-        // provide routing information
-        if (app !== 'admin') {
-            routerLink.push('project');
-        }
-        this.router.navigate(routerLink);
     }
 }
