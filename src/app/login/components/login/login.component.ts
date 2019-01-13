ee9c720f68c3431016fdca8fc59d4b57f5a82cdb
Philip <Philip.Schulz-Klingauf@gentics.com>
2019-01-13 20:42:10 +0100

    animated login screen pageload

--- src/app/login/components/login/login.component.ts
+++ src/app/login/components/login/login.component.ts
@@ -1,26 +1,31 @@
-import { Component, OnDestroy } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs/Subscription';
 
 import { ApplicationStateService } from '../../../state/providers/application-state.service';
 import { AuthEffectsService } from '../../providers/auth-effects.service';
 
-
 @Component({
     selector: 'mesh-login',
     templateUrl: './login.component.html',
     styleUrls: ['./login.scss']
 })
-export class LoginComponent implements OnDestroy {
-
+export class LoginComponent implements AfterViewInit, OnDestroy {
     username = '';
     password = '';
 
+    /** CSS class string defining logo animation state */
+    animclasslogo = 'cube__face--front';
+    animclassimg = '';
+    animclassform = '';
+
     private subscription: Subscription;
 
-    constructor(private appState: ApplicationStateService,
-                private authEffects: AuthEffectsService,
-                private router: Router) {
+    constructor(
+        private appState: ApplicationStateService,
+        private authEffects: AuthEffectsService,
+        private router: Router
+    ) {
         this.subscription = this.appState
             .select(state => state.auth.loggedIn)
             .filter(isLoggedIn => isLoggedIn)
@@ -30,11 +35,41 @@ export class LoginComponent implements OnDestroy {
             });
     }
 
-    onSubmit(): void {
-        this.authEffects.login(this.username, this.password);
+    ngAfterViewInit(): void {
+        setTimeout(() => {
+            this.animate();
+        }, 0);
     }
 
     ngOnDestroy(): void {
         this.subscription.unsubscribe();
     }
+
+    onSubmit(): void {
+        this.authEffects.login(this.username, this.password);
+    }
+
+    animate(): void {
+        this.animclasslogo = ELogoAnimClass.ANGLE;
+        this.animclassimg = ELogoAnimClass.ROTATE;
+
+        setTimeout(() => {
+            this.animclasslogo += ' ' + ELogoAnimClass.FADE_OUT;
+        }, 250);
+
+        setTimeout(() => {
+            this.animclassimg += ' ' + ELogoAnimClass.FADE_IN;
+        }, 280);
+
+        setTimeout(() => {
+            this.animclassform = ELogoAnimClass.FADE_IN;
+        }, 300);
+    }
+}
+
+enum ELogoAnimClass {
+    ANGLE = 'show-angle',
+    FADE_IN = 'fade-in',
+    FADE_OUT = 'fade-out',
+    ROTATE = 'rotate'
 }
