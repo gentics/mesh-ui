d3f588925e977a53df20e4c1e377916963267c99
Philip <Philip.Schulz-Klingauf@gentics.com>
2019-01-18 16:39:55 +0100

    refactoring

--- src/app/editor/providers/opener.service.ts
+++ src/app/editor/providers/opener.service.ts
@@ -3,14 +3,14 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class OpenerService {
     /** open tab data */
-    opener: MeshOpener[] = [];
+    browserTabs: MeshOpener[] = [];
 
     /**
      * Open new tab
      * @param url of the new tab
      */
     open(url: string): void {
-        this.opener.push({
+        this.browserTabs.push({
             url,
             opener: window.open(url)
         });
@@ -20,7 +20,7 @@ export class OpenerService {
      * Reload all opened tabs
      */
     reload(): void {
-        this.opener.forEach(opener => (opener.opener.location = opener.url));
+        this.browserTabs.forEach(opener => (opener.opener.location = opener.url));
     }
 }
 
