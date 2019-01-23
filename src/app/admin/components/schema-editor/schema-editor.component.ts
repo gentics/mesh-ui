cf0d598fa6f7f2ed4edde015fe9db239d83ad07f
Philip <Philip.Schulz-Klingauf@gentics.com>
2019-01-23 11:41:18 +0100

    WIP: refactoring

--- src/app/admin/components/schema-editor/schema-editor.component.ts
+++ src/app/admin/components/schema-editor/schema-editor.component.ts
@@ -20,6 +20,8 @@ import { AdminSchemaEffectsService } from '../../providers/effects/admin-schema-
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SchemaEditorComponent implements OnInit, OnDestroy {
+    // PROPERTIES //////////////////////////////////////////////////////////////////////////////
+
     formGroup: FormGroup;
 
     schema: SchemaUpdateRequest | Schema;
@@ -28,46 +30,44 @@ export class SchemaEditorComponent implements OnInit, OnDestroy {
 
     allMicroschemas$: Observable<Microschema[]>;
 
-    allowedBarValue: any = '';
-
-    allowedBarStrings: Array<Set<string>> = [];
+    allowValues: Array<Set<string>> = [];
 
     schemaTypes: Array<{ value: SchemaFieldType; label: string }> = [
         {
-            value: 'node',
-            label: 'Node'
+            value: 'binary',
+            label: 'Binary'
         },
         {
             value: 'boolean',
             label: 'Boolean'
         },
         {
-            value: 'string',
-            label: 'String'
-        },
-        {
-            value: 'number',
-            label: 'Number'
-        },
-        {
             value: 'date',
             label: 'Date'
-        },
-        {
-            value: 'html',
-            label: 'HTML'
         },
         {
             value: 'micronode',
             label: 'Micronode'
         },
         {
-            value: 'binary',
-            label: 'Binary'
+            value: 'node',
+            label: 'Node'
+        },
+        {
+            value: 'number',
+            label: 'Number'
+        },
+        {
+            value: 'html',
+            label: 'HTML'
         },
         {
             value: 'list',
             label: 'List'
+        },
+        {
+            value: 'string',
+            label: 'String'
         }
     ];
 
@@ -89,14 +89,32 @@ export class SchemaEditorComponent implements OnInit, OnDestroy {
 
     private destroyed$: Subject<void> = new Subject();
 
+    // CONSTRUCTOR //////////////////////////////////////////////////////////////////////////////
+
     constructor(
         private entities: EntitiesService,
         private adminSchemaEffects: AdminSchemaEffectsService,
         private formBuilder: FormBuilder
     ) {}
 
-    // LIFECYCLE HOOKS
+    // LIFECYCLE HOOKS //////////////////////////////////////////////////////////////////////////////
+
     ngOnInit(): void {
+        this.loadComponentData();
+        this.formGroupInit();
+    }
+
+    ngOnDestroy(): void {
+        this.destroyed$.next();
+        this.destroyed$.complete();
+    }
+
+    // MANAGE COMPONENT DATA //////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * Trigger loading of required data and assign data streams to component properties
+     */
+    protected loadComponentData(): void {
         // assign data streams
         this.allSchemas$ = this.entities
             .selectAllSchemas()
@@ -107,8 +125,14 @@ export class SchemaEditorComponent implements OnInit, OnDestroy {
         // request data
         this.adminSchemaEffects.loadSchemas();
         this.adminSchemaEffects.loadMicroschemas();
+    }
 
-        // initialize form
+    // MANAGE FORM //////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * Initialize form with empty/default data and listen to changes
+     */
+    protected formGroupInit(): void {
         this.formGroup = this.formBuilder.group({
             name: ['', Validators.required],
             container: [false],
@@ -152,75 +176,24 @@ export class SchemaEditorComponent implements OnInit, OnDestroy {
                                 this.schema.fields[index] &&
                                 this.schema.fields[index].listType !== field.listType)
                         ) {
-                            this.allowedStringsOnClear(index);
+                            this.allowValuesClearAt(index);
                             this.propertyPurge(field, index, 'allow');
                         }
 
                         // if of type string, trigger search bar functionality
                         if (field.type === 'string' || field.listType === 'string') {
-                            this.allowedStringsOnChange(index);
+                            this.allowValuesOnStringInputChangeAt(index);
                         }
 
                         // if allow property should exist, assign appropriate data
                         Object.assign(schemaField, {
-                            allow: this.allowedBarStrings[index] ? Array.from(this.allowedBarStrings[index]) : null
+                            allow: this.allowValues[index] ? Array.from(this.allowValues[index]) : null
                         });
 
                         // if field.allow proeprty is an empty array, clean it up
                         if (field.allow && field.allow.length && field.allow === 0) {
                             this.propertyPurge(field, index, 'allow');
                         }
-
-                        // switch (field.type) {
-                        //     case 'node':
-                        //         if (field.allow) {
-                        //             Object.assign(schemaField, { allow: field.allow });
-                        //         }
-                        //         break;
-                        //     case 'micronode':
-                        //         if (field.listType !== 'micronode') {
-                        //             // clear properties not allowed for field of type 'micronode'
-                        //             this.propertyPurge(field, index, 'listType');
-                        //         }
-                        //         if (field.allow) {
-                        //             Object.assign(schemaField, { allow: field.allow });
-                        //         }
-                        //         break;
-                        //     case 'list':
-                        //         // // clear properties not allowed for field of type 'list'
-                        //         // if (field.listType !== 'node' || 'micronode') {
-                        //         //     this.propertyPurge(field, index, 'allow');
-                        //         // }
-                        //         // if previously field.type has been node or micronode, reset field.allow
-                        //         if (this.schema && this.schema.fields && this.schema.fields[index].type !== 'list') {
-                        //             this.allowedStringsOnClear(index);
-                        //         }
-                        //         Object.assign(schemaField, {
-                        //             allow: this.allowedBarStrings[index]
-                        //                 ? Array.from(this.allowedBarStrings[index])
-                        //                 : null
-                        //         });
-                        //         if (field.listType) {
-                        //             Object.assign(schemaField, { listType: field.listType });
-                        //         }
-                        //         break;
-                        //     case 'string':
-                        //         // if previously field.type has been node or micronode, reset field.allow
-                        //         if (this.schema && this.schema.fields && this.schema.fields[index].type !== 'string') {
-                        //             this.allowedStringsOnClear(index);
-                        //         }
-                        //         // clear properties not allowed for field of type 'micronode'
-                        //         this.propertyPurge(field, index, 'listType');
-                        //         this.allowedStringsOnChange(index);
-                        //         Object.assign(schemaField, {
-                        //             allow: this.allowedBarStrings[index]
-                        //                 ? Array.from(this.allowedBarStrings[index])
-                        //                 : null
-                        //         });
-                        //         break;
-                        //     default:
-                        //         break;
-                        // }
 
                         return schemaField;
                     })
@@ -229,13 +202,13 @@ export class SchemaEditorComponent implements OnInit, OnDestroy {
             });
     }
 
-    ngOnDestroy(): void {
-        this.destroyed$.next();
-        this.destroyed$.complete();
-    }
+    // MANAGE SCHEMA.FIELD[] ENTRIES //////////////////////////////////////////////////////////////////////////////
 
-    createNewField(): FormGroup {
-        this.allowedBarStrings.push(new Set<string>());
+    /**
+     * Initialize a new FormGroup instance and its related data analog to schema.field type
+     */
+    protected createNewField(): FormGroup {
+        this.allowValues.push(new Set<string>());
         return this.formBuilder.group({
             name: ['', Validators.required],
             label: ['', Validators.required],
@@ -258,6 +231,112 @@ export class SchemaEditorComponent implements OnInit, OnDestroy {
         this.fieldRemoveAt(this.schemaFields.length - 1);
     }
 
+    // MANAGE SCHEMA.FIELD[].ALLOW VALUES //////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * add value to schema.fields[] allow[]
+     * @param index of schema.fields[]
+     * @param value string to be added
+     * */
+    allowValueAddAt(index: number, value: string): void {
+        // update allowed data
+        this.allowValues[index].add(value);
+        // update form
+        this.formGroup.updateValueAndValidity();
+    }
+
+    /**
+     * remove value from schema.fields[] allow[]
+     * @param index of schema.fields[]
+     * @param value string to be removed
+     * */
+    allowValueRemoveAt(index: number, value: string): void {
+        // update allowed data
+        this.allowValues[index].delete(value);
+        // update form
+        this.formGroup.updateValueAndValidity();
+    }
+
+    /**
+     * change value of schema.fields[] allow[]
+     * @param index of schema.fields[]
+     * @param value string to be changed
+     * @param action indicator: true = add, false = remove
+     */
+    allowValuesChangeAt(index: number, value: string, action: boolean): void {
+        if (action === true) {
+            this.allowValueAddAt(index, value);
+        } else {
+            this.allowValueRemoveAt(index, value);
+        }
+    }
+
+    allowValueRemoveLastAt(index: number): void {
+        // if input bar has text, which is not yet converted to chip, don't redirect backspace key
+        const form = this.formGroup.value as Schema;
+        const inputText = form.fields && form.fields[index] && form.fields[index].allow;
+        if (inputText && inputText.length) {
+            return;
+        }
+
+        const allowValues = this.allowValues[index];
+        if (!allowValues) {
+            return;
+        }
+        const lastValue = Array.from(allowValues).pop();
+        if (!lastValue) {
+            return;
+        }
+        this.allowValueRemoveAt(index, lastValue);
+    }
+
+    /**
+     * Remove all entries from allowed string values of schema field
+     * @param index of field to be cleared
+     */
+    allowValuesClearAt(index: number): void {
+        // clear data
+        this.allowValues[index] = new Set();
+
+        // clear form values
+        const newForm = simpleCloneDeep(this.formGroup.value);
+        const newField = newForm.fields[index];
+        if (!newField.allow) {
+            return;
+        }
+        newField.allow = '';
+        this.formGroup.patchValue(newForm);
+    }
+
+    allowValuesContainsAt(index: number, value: string): boolean {
+        return this.allowValues[index].has(value);
+    }
+
+    allowValueOnStringInputAddAt(index: number): void {
+        const newForm = simpleCloneDeep(this.formGroup.value);
+        const newField = newForm.fields[index];
+        // update mesh chip array cleaned from forbiddden characters
+        this.allowValues[index].add(newField.allow.replace(' ', '').replace(',', ''));
+        // empty field after new value is displayed as chip
+        newField.allow = '';
+        // assign new value to form data
+        this.formGroup.patchValue(newForm);
+    }
+
+    allowValuesOnStringInputChangeAt(index: number): void {
+        const newForm = simpleCloneDeep(this.formGroup.value);
+        const newField = newForm.fields[index];
+        if (!newField.allow) {
+            return;
+        }
+        // if input value is seperated by space or comma, then add as chip
+        if (newField.allow.indexOf(',') > -1 || newField.allow.indexOf(' ') > -1) {
+            this.allowValueOnStringInputAddAt(index);
+        }
+    }
+
+    // SCHEMA MAIN BUTTON //////////////////////////////////////////////////////////////////////////////
+
     schemaCreate(): void {
         console.log('!!! schemaCreate()');
     }
@@ -266,151 +345,7 @@ export class SchemaEditorComponent implements OnInit, OnDestroy {
         console.log('!!! schemaDelete()');
     }
 
-    /**
-     * add entry to schema.fields[] allow[]
-     * @param index of schema.fields[]
-     * @param entry string to be added
-     * */
-    schemaFieldAllowEntryAdd(index: number, entry: string): void {
-        // update allowed data
-        this.allowedBarStrings[index].add(entry);
-        // update form
-        this.formGroup.updateValueAndValidity();
-        // const newForm = simpleCloneDeep(this.formGroup.value) as SchemaUpdateRequest;
-        // const newField = newForm.fields[index] as SchemaField;
-        // // if array not yet existing, create it
-        // if (!newField.allow) {
-        //     newField.allow = [];
-        // }
-        // // make new set from array to keep entries unique
-        // const allowSet = new Set<string>(newField.allow);
-        // allowSet.add(entry);
-        // newField.allow = Array.from<string>(allowSet);
-        // // assign new value to form data
-        // this.formGroup.patchValue(newForm);
-    }
-
-    /**
-     * remove entry to schema.fields[] allow[]
-     * @param index of schema.fields[]
-     * @param entry string to be removed
-     * */
-    schemaFieldAllowEntryRemove(index: number, entry: string): void {
-        // update allowed data
-        this.allowedBarStrings[index].delete(entry);
-        // update form
-        this.formGroup.updateValueAndValidity();
-        // // update form data
-        // const newForm = simpleCloneDeep(this.formGroup.value) as SchemaUpdateRequest;
-        // const newField = newForm.fields[index] as SchemaField;
-
-        // if (!newField.allow || !(newField.allow instanceof Array)) {
-        //     return;
-        // }
-        // // remove entry
-        // newField.allow = newField.allow.filter(item => item !== entry);
-        // // assign new value to form data
-        // this.formGroup.patchValue(newForm);
-    }
-
-    /**
-     * change entry of schema.fields[] allow[]
-     * @param index of schema.fields[]
-     * @param entry string to be changed
-     * @param action indicator: true = add, false = remove
-     */
-    schemaFieldAllowEntryChange(index: number, entry: string, action: boolean): void {
-        if (action === true) {
-            this.schemaFieldAllowEntryAdd(index, entry);
-        } else {
-            this.schemaFieldAllowEntryRemove(index, entry);
-        }
-    }
-
-    allowedStringsOnAdd(index: number): void {
-        const newForm = simpleCloneDeep(this.formGroup.value);
-        const newField = newForm.fields[index];
-        // update mesh chip array cleaned from forbiddden characters
-        this.allowedBarStrings[index].add(newField.allow.replace(' ', '').replace(',', ''));
-        // empty field after new value is displayed as chip
-        newField.allow = '';
-        // assign new value to form data
-        this.formGroup.patchValue(newForm);
-    }
-
-    /**
-     * Remove entry from allowed string values of schema field
-     * @param entry to be removed
-     */
-    allowedStringsOnRemove(index: number, entry: string): void {
-        // remove from entries array
-        this.allowedBarStrings[index].delete(entry);
-        // update form
-        this.formGroup.updateValueAndValidity();
-    }
-
-    allowedStringsOnRemoveLast(index: number): void {
-        // if input bar has text, which is not yet converted to chip, don't redirect backspace key
-        const form = this.formGroup.value as Schema;
-        const inputText = form.fields && form.fields[index] && form.fields[index].allow;
-        if (inputText && inputText.length) {
-            return;
-        }
-
-        const allowedBarStrings = this.allowedBarStrings[index];
-        if (!allowedBarStrings) {
-            return;
-        }
-        const lastValue = Array.from(allowedBarStrings).pop();
-        if (!lastValue) {
-            return;
-        }
-        this.allowedStringsOnRemove(index, lastValue);
-    }
-
-    allowedStringsOnChange(index: number): void {
-        const newForm = simpleCloneDeep(this.formGroup.value);
-        const newField = newForm.fields[index];
-        if (!newField.allow) {
-            return;
-        }
-        // if input value is seperated by space or comma, then add as chip
-        if (newField.allow.indexOf(',') > -1 || newField.allow.indexOf(' ') > -1) {
-            this.allowedStringsOnAdd(index);
-        }
-    }
-
-    /**
-     * Remove all entries from allowed string values of schema field
-     * @param index of field to be cleared
-     */
-    allowedStringsOnClear(index: number): void {
-        this.allowedBarStrings[index] = new Set();
-        const newForm = simpleCloneDeep(this.formGroup.value);
-        const newField = newForm.fields[index];
-
-        if (!newField.allow) {
-            return;
-        }
-        newField.allow = '';
-
-        // assign new value to form data
-        this.formGroup.patchValue(newForm);
-    }
-
-    allowedBarStringsContains(index: number, value: string): boolean {
-        // if (!this.formGroup.value) {
-        //     return false;
-        // }
-        // const form = this.formGroup.value as Schema;
-        // return form.fields &&
-        //     form.fields[index] &&
-        //     form.fields[index].allow &&
-        //     form.fields[index].allow!.indexOf(value) > -1
-        //         ? true
-        //         : false;
-        return this.allowedBarStrings[index].has(value);
-    }
+    // PRIVATE METHODS //////////////////////////////////////////////////////////////////////////////
 
     private propertyPurge(field: any, index: number, property: keyof SchemaField): void {
         delete field[property];
