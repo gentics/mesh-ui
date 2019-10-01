c25b50d2dd0fa9121536b7807f64363aa2cadbe7
Philip Viktor Schulz-Klingauf <psk@schulz-klingauf.de>
2019-10-01 16:25:29 +0200

    Improve styles (#247)
    * improving permission styles
    
    * permissions module now using shared module
    
    * added translations
    
    * added more translations because some other dev supposed to didn't
    
    * restyled permissions apply recursive button
    
    * changed admin breadcrumb colors
    
    * minor style changes

--- src/app/admin/permissions/permissions.module.ts
+++ src/app/admin/permissions/permissions.module.ts
@@ -1,31 +1,25 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { AccordionModule } from 'primeng/accordion';
 import { ButtonModule } from 'primeng/button';
 import { CheckboxModule } from 'primeng/checkbox';
 import { PaginatorModule } from 'primeng/paginator';
 import { TreeTableModule } from 'primeng/treetable';
 
-import { IconCheckboxComponent } from './icon-checkbox/icon-checkbox.component';
+import { SharedModule } from '../../shared/shared.module';
+
 import { NodePermissionsComponent } from './node-permissions/node-permissions.component';
 import { PermissionsComponent } from './permissions.component';
 import { SimplePermissionsComponent } from './simple-permissions/simple-permissions.component';
 import { TagPermissionsComponent } from './tag-permissions/tag-permissions.component';
 
 @NgModule({
-    declarations: [
-        PermissionsComponent,
-        IconCheckboxComponent,
-        TagPermissionsComponent,
-        NodePermissionsComponent,
-        SimplePermissionsComponent
-    ],
+    declarations: [PermissionsComponent, TagPermissionsComponent, NodePermissionsComponent, SimplePermissionsComponent],
     imports: [
         AccordionModule,
         CheckboxModule,
         CommonModule,
-        FormsModule,
+        SharedModule,
         PaginatorModule,
         TreeTableModule,
         ButtonModule
