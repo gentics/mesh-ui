d1a791dbc48755d143f4526e3f2b8d9bf5c15790
Philipp Gürtler <p.guertler@gentics.com>
2019-10-07 14:54:14 +0200

    Add test

--- testcafe/test/container-contents.ts
+++ testcafe/test/container-contents.ts
@@ -1,16 +1,17 @@
 import { api } from '../api';
 import { navigate } from '../navigate';
 import { containerContents } from '../page-object/editor/container-contents';
+import { paginationControls } from '../page-object/pagination-controls';
 import { topnav } from '../page-object/topnav';
 import { Admin } from '../roles';
-import { selectors } from '../selectors';
+import { inTemporaryFolder } from '../testUtil';
 
 import { schemaEditor } from './admin/schema-editor';
 
 fixture`Container contents`.page(api.baseUrl());
 
 const schemaName = 'dummy';
-test.only('Only assigned schemas are visible', async t => {
+test('Only assigned schemas are visible', async t => {
     await t.useRole(Admin);
 
     await navigate.toAdminSchemaEditorNew();
@@ -31,3 +32,16 @@ test.only('Only assigned schemas are visible', async t => {
         await api.deleteSchema(await api.getSchemaByName(schemaName));
     }
 });
+
+test.only('Current page stays the same after opening Node', async t =>
+    inTemporaryFolder(async folder => {
+        for (let i = 0; i < 50; i++) {
+            await api.createVehicle(folder, `vehicle${i}`);
+        }
+        await t.useRole(Admin);
+        await navigate.toFolder(folder);
+        await paginationControls.goToPage(2);
+        await containerContents.getFirstListItem().open();
+
+        await t.expect(await paginationControls.currentPage()).eql(2, 'Page changed after opening node');
+    }));
