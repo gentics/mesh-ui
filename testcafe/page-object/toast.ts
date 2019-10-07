eefb1b5eee3a1881eae3d5bcebe02aef2e15c2ac
Philipp Gürtler <p.guertler@gentics.com>
2019-10-07 14:27:45 +0200

    Fix number field (#257)
    * Fix rx migration bug
    
    * Fix number field

--- testcafe/page-object/toast.ts
+++ testcafe/page-object/toast.ts
@@ -2,8 +2,16 @@ import { t, Selector } from 'testcafe';
 
 export namespace toast {
     export async function expectErrorMessage(msg: string) {
+        return expectMessage(msg, 'error');
+    }
+
+    export async function expectSuccessMessage(msg: string) {
+        return expectMessage(msg, 'success');
+    }
+
+    async function expectMessage(msg: string, type: string) {
         await t
-            .expect(Selector('gtx-toast div.error').withText(msg).exists)
+            .expect(Selector(`gtx-toast div.${type}`).withText(msg).exists)
             .ok(`Expected toast error message containing "${msg}"`);
     }
 }
